f061927534237db3b941f93c3c2906a3ed6dc544
Nines <921944921@qq.com>
2016-02-27 14:14:17 +0800

    Update rinetd.sh

--- rinetd.sh
+++ rinetd.sh
@@ -5,6 +5,7 @@ wget http://www.boutell.com/rinetd/http/rinetd.tar.gz
 tar zxvf rinetd.tar.gz 
 cd rinetd 
 mkdir -p /usr/man/man8
+yum -y install gcc automake autoconf libtool make
 make && make install
 echo  '0.0.0.0 443 192.184.12.180 443' > /etc/rinetd.conf
 /usr/sbin/rinetd -c /etc/rinetd.conf
