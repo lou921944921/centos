c3f20a73dc9b01035a1d28e46cd2ff29dca26d4a
Nines <921944921@qq.com>
2016-02-27 14:43:24 +0800

    Create ss-libev.sh

---
+++ ss-libev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+yum -y update
+echo  '[librehat-shadowsocks]
+name=Copr repo for shadowsocks owned by librehat
+baseurl=https://copr-be.cloud.fedoraproject.org/results/librehat/shadowsocks/epel-7-$basearch/
+skip_if_unavailable=True
+gpgcheck=1
+gpgkey=https://copr-be.cloud.fedoraproject.org/results/librehat/shadowsocks/pubkey.gpg
+enabled=1
+enabled_metadata=1' > /etc/yum.repos.d/shadowsocks-libev.repo
+yum -y install shadowsocks-libev
+echo '{
+    "server": "0.0.0.0",
+    "server_port": 8388,
+    "local_port": 1080,
+    "password": "123456",
+    "timeout": 600,
+    "method": "aes-256-cfb"
+}' > /etc/shadowsocks-libev/config.json
+ss-redir  -c /etc/shadowsocks-libev/config.json -f /var/run/shadowsocks.pid
+sed -i '2a ss-redir  -c /etc/shadowsocks-libev/config.json -f /var/run/shadowsocks.pid' /etc/rc.local
