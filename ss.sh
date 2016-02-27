7fce52a0791db44e75ca2d886f4b02c1f3afcd51
Nines <921944921@qq.com>
2016-02-27 16:29:21 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-yum -y update
-yum -y install python-setuptools && easy_install pip 
-pip install shadowsocks
+touch /etc/ss-config.json  
 echo '{
     "server": "0.0.0.0",
     "server_port": 8388,
@@ -10,6 +8,10 @@ echo '{
     "timeout": 600,
     "method": "aes-256-cfb"
 }' > /etc/ss-config.json
+chmod 755 /etc/ss-config.json
+yum -y update
+yum -y install python-setuptools && easy_install pip 
+pip install shadowsocks
 ssserver -c /etc/ss-config.json -d start
 sed -i '2a /usr/local/bin/ssserver -c /etc/ss-config.json' /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
