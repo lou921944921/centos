0c1611ba5c637ab7c8c023fe59c92b2fc4c4130c
Nines <921944921@qq.com>
2016-02-27 16:47:41 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -13,6 +13,6 @@ yum -y update
 yum -y install python-setuptools && easy_install pip 
 pip install shadowsocks
 ssserver -c /etc/ss-config.json -d start
-sed -i '2a /usr/local/bin/ssserver -c /etc/ss-config.json' /etc/rc.d/rc.local
+sed -i '2a ssserver -c /etc/ss-config.json -d start' /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
 yum clean all
