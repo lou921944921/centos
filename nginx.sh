deb77982dd1a99f34b3a67b50503d0e7b8af2bca
Nines <921944921@qq.com>
2016-04-17 21:37:08 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -6,20 +6,7 @@ wget http://nginx.org/download/nginx-1.9.14.tar.gz
 tar -zxvf nginx-1.9.14.tar.gz
 cd nginx-1.9.14
 ./configure --prefix=/etc/nginx  --with-stream  && make && make install
-sed -i '$a stream {
-    upstream warframe {
-        server 192.184.12.180:443           max_fails=3 fail_timeout=30s;
-    }
-
-
-    server {
-        listen 443;
-        proxy_connect_timeout 30s;
-        proxy_timeout 30s;
-        proxy_pass warframe;
-    }
-
-}' /etc/nginx/conf/nginx.conf
+sed -i '$a stream {\n    upstream warframe {\n        server 192.184.12.180:443           max_fails=3 fail_timeout=30s;\n    }\n    server {\n        listen 443;\n        proxy_connect_timeout 30s;\n        proxy_timeout 30s;\n        proxy_pass warframe;\n    }\n\n}' /etc/nginx/conf/nginx.conf
 /etc/nginx/sbin/nginx -c /etc/nginx/conf/nginx.conf
 sed -i '1a /etc/nginx/sbin/nginx -c /etc/nginx/conf/nginx.conf' /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
