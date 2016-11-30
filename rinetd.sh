3e8d3023716ca16a5625381260b5754d42ef0f00
Nines <921944921@qq.com>
2016-11-30 15:26:07 +0800

    Update rinetd.sh

--- rinetd.sh
+++ rinetd.sh
@@ -8,6 +8,7 @@ cd rinetd
 mkdir -p /usr/man/man8
 yum -y install gcc automake autoconf libtool make
 make && make install
+touch /etc/rinetd.conf
 echo  '0.0.0.0 443 192.184.12.180 443' > /etc/rinetd.conf
 /usr/sbin/rinetd -c /etc/rinetd.conf
 sed -i '1a /usr/sbin/rinetd -c /etc/rinetd.conf' /etc/rc.d/rc.local
